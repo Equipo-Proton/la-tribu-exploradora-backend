5ec9cf2452b20b5a64363d5587820fe1b516c943
Guillermo García <guillermieres97@gmail.com>
2022-10-01 23:45:11 +0200

    implementation of play

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,8 @@ Route::post('/login', [UserController::class, 'login'])->name('login');
 
 // Rutas del profesor admin
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::patch('/play', [UserController::class, 'play']);
+    Route::get('/playvalue', [UserController::class, 'getPlayvalue']);
+    Route::patch('/play', [UserController::class, 'play'])->middleware('isadmin');
     Route::post('/register', [UserController::class, 'register'])->name('register')->middleware('isadmin');
     Route::get('/users', [UserController::class, 'getUsers'])->name('users')->middleware('isadmin');
     Route::get('/userprofile/{id}', [UserController::class, 'userProfile'])->name('userProfile')->middleware('isadmin');
