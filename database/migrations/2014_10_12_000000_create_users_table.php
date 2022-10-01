4b6383fd6dcc9cce95ee1dee4fc3090e10638ca7
Guillermo García <guillermieres97@gmail.com>
2022-10-01 11:11:19 +0200

    clean code and session laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -23,6 +24,7 @@ return new class extends Migration
             $table->boolean('superAdmin')->default(false);
             $table->integer('teacher')->references('id')->on('users')->nullable();
             $table->string('showPassword');
+
             $table->rememberToken();
             $table->timestamps();
         });
